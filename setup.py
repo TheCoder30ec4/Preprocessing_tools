1e84a2401a896f349b5b5af8b870c0199def2dc7
Antriksh Arya <antriksh0704@gmail.com>
2024-07-22 11:36:53 +0530

    editing

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Data Preprocessing library for Data Science and Machine Learning.'
+LONG_DESCRIPTION = 'A package that provides all the data preprocessing tools important to Data scienece and ML tasks in one place'
+
+# Setting up
+setup(
+    name="preprocessing_tools",
+    version=VERSION,
+    author="Antriksh Arya",
+    author_email="<antriksh0704@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['scikit-learn', 'numpy', 'pandas'],
+    keywords=['python', 'data', 'preprocessing', 'data science', 'data analysis', 'machine learning'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
